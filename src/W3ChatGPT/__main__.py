95953f93f67b91c9b234a88f4fd6b766d7a47d19
Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>
2023-02-05 21:19:33 -0800

    Update __main__.py
    Signed-off-by: Common Vulnerabilities and Exploits Database <84289402+cvedb@users.noreply.github.com>

--- src/W3ChatGPT/__main__.py
+++ src/W3ChatGPT/__main__.py
@@ -1,123 +1,109 @@
-import argparse
-import sys
 
-from W3ChatGpt.Official import Chatbot
+import json
+import textwrap
+from os.path import exists
+from sys import argv
+
+from W3ChatGPT.W3ChatGPT import Chatbot
 
 
-def main():
-    print(
-        """
-    ChatGPT - A command-line interface to OpenAI's ChatGPT (https://chat.openai.com/chat)
-    Repo: github.com/w3security/W3Chat
-    """,
-    )
-    print("Type '!help' to show a full list of commands")
-    print("Press enter twice to submit your question.\n")
-
-    def get_input(prompt):
-        """
-        Multi-line input function
-        """
-        # Display the prompt
-        print(prompt, end="")
-
-        # Initialize an empty list to store the input lines
-        lines = []
-
-        # Read lines of input until the user enters an empty line
-        while True:
-            line = input()
-            if line == "":
-                break
-            lines.append(line)
-
-        # Join the lines, separated by newlines, and store the result
-        user_input = "\n".join(lines)
-
-        # Return the input
-        return user_input
-
-    def chatbot_commands(cmd: str) -> bool:
-        """
-        Handle chatbot commands
-        """
-        if cmd == "!help":
-            print(
-                """
-            !help - Display this message
-            !rollback - Rollback chat history
-            !reset - Reset chat history
-            !prompt - Show current prompt
-            !save_c <conversation_name> - Save history to a conversation
-            !load_c <conversation_name> - Load history from a conversation
-            !save_f <file_name> - Save all conversations to a file
-            !load_f <file_name> - Load all conversations from a file
-            !exit - Quit chat
-            """,
-            )
-        elif cmd == "!exit":
-            exit()
-        elif cmd == "!rollback":
-            chatbot.rollback(1)
-        elif cmd == "!reset":
-            chatbot.reset()
-        elif cmd == "!prompt":
-            print(chatbot.prompt.construct_prompt(""))
-        elif cmd.startswith("!save_c"):
-            chatbot.save_conversation(cmd.split(" ")[1])
-        elif cmd.startswith("!load_c"):
-            chatbot.load_conversation(cmd.split(" ")[1])
-        elif cmd.startswith("!save_f"):
-            chatbot.conversations.save(cmd.split(" ")[1])
-        elif cmd.startswith("!load_f"):
-            chatbot.conversations.load(cmd.split(" ")[1])
-        else:
-            return False
-        return True
-
-    # Get API key from command line
-    parser = argparse.ArgumentParser()
-    parser.add_argument(
-        "--api_key",
-        type=str,
-        required=True,
-        help="OpenAI API key",
-    )
-    parser.add_argument(
-        "--stream",
-        action="store_true",
-        help="Stream response",
-    )
-    parser.add_argument(
-        "--temperature",
-        type=float,
-        default=0.5,
-        help="Temperature for response",
-    )
-    args = parser.parse_args()
-    # Initialize chatbot
-    chatbot = Chatbot(api_key=args.api_key)
-    # Start chat
+def get_input(prompt):
+    # prompt for input
+    lines = []
+    print(prompt, end="")
     while True:
-        try:
-            prompt = get_input("\nUser:\n")
-        except KeyboardInterrupt:
-            print("\nExiting...")
-            sys.exit()
-        if prompt.startswith("!"):
-            if chatbot_commands(prompt):
-                continue
-        if not args.stream:
-            response = chatbot.ask(prompt, temperature=args.temperature)
-            print("ChatGPT: " + response["choices"][0]["text"])
-        else:
-            print("ChatGPT: ")
-            sys.stdout.flush()
-            for response in chatbot.ask_stream(prompt, temperature=args.temperature):
-                print(response, end="")
-                sys.stdout.flush()
-            print()
+        line = input()
+        if line == "":
+            break
+        lines.append(line)
+
+    # Join the lines, separated by newlines, and print the result
+    user_input = "\n".join(lines)
+    # print(user_input)
+    return user_input
 
 
 if __name__ == "__main__":
-    main()
+    print(
+        """
+    ChatGPT - A command-line interface to OpenAI's ChatGPT (https://chat.openai.com/chat)
+    Repo: github.com/w3security/W3ChatGPT
+    """,
+    )
+    print("Type '!help' to show commands")
+    print("Press enter twice to submit your question.\n")
+
+    if exists("config.json"):
+        with open("config.json", encoding="utf-8") as f:
+            config = json.load(f)
+        chatbot = Chatbot(config)
+    else:
+        print("Please create and populate config.json to continue")
+        exit()
+
+    while True:
+        prompt = get_input("\nYou:\n")
+        if prompt.startswith("!"):
+            if prompt == "!help":
+                print(
+                    """
+                !help - Show this message
+                !reset - Forget the current conversation
+                !refresh - Refresh the session authentication
+                !rollback - Rollback the conversation by 1 message
+                !config - Show the current configuration
+                !exit - Exit the program
+                """,
+                )
+                continue
+            elif prompt == "!reset":
+                chatbot.reset_chat()
+                print("Chat session reset.")
+                continue
+            elif prompt == "!refresh":
+                chatbot.refresh_session()
+                print("Session refreshed.\n")
+                continue
+            elif prompt == "!rollback":
+                chatbot.rollback_conversation()
+                print("Chat session rolled back.")
+                continue
+            elif prompt == "!config":
+                print(json.dumps(config, indent=4))
+                continue
+            elif prompt == "!exit":
+                break
+
+        if "--text" not in argv:
+            messages = []
+            lines_printed = 0
+
+            try:
+                print("Chatbot: ")
+                formatted_parts = []
+                for message in chatbot.get_chat_response(prompt, output="stream"):
+                    # Split the message by newlines
+                    message_parts = message["message"].split("\n")
+
+                    # Wrap each part separately
+                    formatted_parts = []
+                    for part in message_parts:
+                        formatted_parts.extend(textwrap.wrap(part, width=80))
+                        for formatted_line in formatted_parts:
+                            if len(formatted_parts) > lines_printed + 1:
+                                print(formatted_parts[lines_printed])
+                                lines_printed += 1
+                print(formatted_parts[lines_printed])
+            except Exception as e:
+                print("Something went wrong!")
+                print(e)
+                continue
+        else:
+            try:
+                print("Chatbot: ")
+                message = chatbot.get_chat_response(prompt)
+                print(message["message"])
+            except Exception as e:
+                print("Something went wrong!")
+                print(e)
+                continue
